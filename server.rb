a50f878f51e7c905d4bea5876476959c692fa0b1
Scott Opell <me@scottopell.com>
2014-11-21 15:55:12 -0500

    adds classes to this

--- server.rb
+++ server.rb
@@ -2,18 +2,32 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default, :test, :development)
 
+class MenuItem
+  def initialize name, price, calories=nil, protein=nil
+    @name = name
+    @price = price
+    @calories = calories
+    @protein = protein
+  end
+
+  def to_html
+    "<li>#{@name} - $#{@price} | #{@calories} | #{@protein} </li>"
+  end
+end
+
 def taco_bell
   menu_url = 'http://www.fastfoodmenuprices.com/taco-bell-prices/'
   page = Nokogiri::HTML(open(menu_url))
-  menu = {}
+  menu = []
   page.css('tbody.row-hover tr').collect do |el|
     if el.css('.column-2').text != '' ||
        el.css('.column-1').text == '' ||
        el.css('.column-3').text == ''
       next
     end
+    name = el.css('.column-1').text
     price = el.css('.column-3').text[1..-1].to_f
-    menu[el.css('.column-1').text] = price
+    menu << MenuItem.new(name, price)
   end
   menu
 end
@@ -21,6 +35,6 @@ end
 get '/' do
   menu = taco_bell
   body = '<h1>Taco Bell</h1>''<ul>'
-  menu.each { |item, price| body << "<li>#{item} $#{price}</li>" }
+  menu.each { |item| body << item.to_html }
   body << '</ul>'
 end
