e92c57394c24fcd8ed6b3da58ea8ad2129c33cc6
Scott Opell <me@scottopell.com>
2014-11-21 15:48:27 -0500

    inital taco bell menu parsing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
+require 'open-uri'
 
 gem 'sinatra'
+gem 'nokogiri'
+
+gem 'pry', group: :development
--- server.rb
+++ server.rb
@@ -2,10 +2,29 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default, :test, :development)
 
+def taco_bell
+  menu_url = 'http://www.fastfoodmenuprices.com/taco-bell-prices/'
+  page = Nokogiri::HTML(open(menu_url))
+  menu = {}
+  page.css('tbody.row-hover tr').collect do |el|
+    if el.css('.column-2').text != '' ||
+       el.css('.column-1').text == '' ||
+       el.css('.column-3').text == ''
+      next
+    end
+    price = el.css('.column-3').text[1..-1].to_f
+    menu[el.css('.column-1').text] = price
+  end
+  menu
+end
+
 get '/hello' do
   "world"
 end
 
 get '/' do
-  '<h1>Taco Bell</h1>'
+  menu = taco_bell
+  body = '<ul>'
+  menu.each { |item, price| body << "<li>#{item} $#{price}</li>" }
+  body << '</ul>'
 end
