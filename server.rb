802dd73bec105f606483d07972ff26b0954d046b
Scott Opell <me@scottopell.com>
2014-11-21 17:30:18 -0500

    implements a pseudo-knapsack optimizing for a single nutritional fact

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 Bundler.require(:default, :test, :development)
 
 class TacoBell
+  INDIANA_STATE_TAX = 1.07
   include Singleton
 
   def self.populate_menu
@@ -17,7 +18,7 @@ class TacoBell
         next
       end
       name = el.css('.column-1').text
-      price = el.css('.column-3').text[1..-1].to_f
+      price = el.css('.column-3').text[1..-1].to_f * INDIANA_STATE_TAX
       mi = MenuItem.new(name, price)
       mi.get_nutritional_info
       @menu << mi
@@ -34,9 +35,29 @@ class TacoBell
     end
     @menu
   end
+
+  def self.best_for wallet, criteria=:calories
+    running_total = 0.00
+    sack = []
+    if @menu.nil?
+      populate_menu
+    end
+
+    sorted = @menu.select{ |mi| !mi.calories.nil? }.sort_by{ |mi| mi.calories }.reverse
+
+    sorted.each do |mi|
+      while (running_total + mi.price) < wallet do
+        sack << mi
+        running_total += mi.price
+      end
+    end
+    sack
+  end
 end
 
 class MenuItem
+  attr_accessor :name, :price, :calories, :protein
+
   def initialize name, price, calories=nil, protein=nil
     @name = name
     @price = price
@@ -45,7 +66,8 @@ class MenuItem
   end
 
   def to_html
-    "<li>#{@name} - $#{@price} | #{@calories} | #{@protein} </li>"
+    price = sprintf("%01.2f", @price)
+    "<tr><td>#{@name}</td><td>$#{price}</td><td>#{@calories}</td><td>#{@protein}</td></tr>"
   end
 
   def get_nutritional_info
@@ -54,8 +76,8 @@ class MenuItem
     @@page ||= Nokogiri::HTML(open(menu_url))
     @@page.css('table#nutrInfo tr').each do |el|
       if @@jarow.getDistance(el.css('th').text, @name) >= 0.8
-        @calories = el.css('td:eq(2)').text
-        @protein =  el.css('td:eq(12)').text
+        @calories = el.css('td:eq(2)').text.to_i
+        @protein =  el.css('td:eq(12)').text.to_i
         return
       end
     end
@@ -64,8 +86,28 @@ end
 
 
 get '/' do
-  body = '<h1>Taco Bell</h1>''<ul>'
-  body << '<li> NAME - PRICE | CALORIES | PROTEIN</li>'
+  body = '<h1>Taco Bell</h1>'
+  body << '<table>'
+  body << '<thead><td>NAME</td><td>PRICE</td><td>CALORIES</td><td>PROTEIN</td></thead>'
+  body << '<tbody>'
   TacoBell.menu.each { |item| body << item.to_html }
-  body << '</ul>'
+  body << '</tbody>'
+  body << '</table>'
+end
+
+get '/best_for' do
+  wallet_amount = params[:wallet].to_f
+
+  body = '<h1>Taco Bell</h1>'
+  body << "<h3>Best for #{wallet_amount}</h3>"
+  body << '<table>'
+  body << '<thead><td>NAME</td><td>PRICE</td><td>CALORIES</td><td>PROTEIN</td></thead>'
+  body << '<tbody>'
+  TacoBell.best_for(wallet_amount).each { |item| body << item.to_html }
+  body << '</tbody>'
+  body << '</table>'
+end
+
+get '/favicon.ico' do
+  File.read('favicon.ico')
 end
